e1a5f6e05db45826944452352301986f177b6aff
User876543210 <sksakil545433@gmail.com>
2024-08-10 12:55:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "academic-guendolen-akasuflim2-a963db9f.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
