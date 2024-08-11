98a3ffe0765ec67224b5ebd088ecf4e447d7aa84
User876543210 <sksakil545433@gmail.com>
2024-08-11 23:36:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://vj-file-store-5f6a.onrender.com")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
