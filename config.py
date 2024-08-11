921151c4d7e6980563e58101134a7da6f56c2125
User876543210 <sksakil545433@gmail.com>
2024-08-11 23:40:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ import re
 import os
 from os import environ
 from Script import script
-
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
